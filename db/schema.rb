3df406ff70fc93d06e2d772d8d89b74d899046a7
Shuji Watanabe <shuji.w6e@gmail.com>
2012-10-23 16:44:45 +0900

    [#38241015] Productに価格を追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121023071505) do
+ActiveRecord::Schema.define(:version => 20121023072637) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
     t.text     "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
+    t.integer  "price",       :default => 0
   end
 
 end
