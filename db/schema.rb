e939ec076681b466e79ed474c7f35ba822a3d48a
Shuji Watanabe <shuji.w6e@gmail.com>
2012-10-23 17:21:06 +0900

    [#38241015] 商品画像をアップロードできるようにした

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121023072637) do
+ActiveRecord::Schema.define(:version => 20121023080328) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20121023072637) do
     t.datetime "created_at",                 :null => false
     t.datetime "updated_at",                 :null => false
     t.integer  "price",       :default => 0
+    t.string   "image"
   end
 
 end
