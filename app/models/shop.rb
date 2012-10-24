7cc133224c14dd8ae99e32ce5ef2d084fc4e6917
Shuji Watanabe <shuji.w6e@gmail.com>
2012-10-24 13:54:06 +0900

    [#38317601] ShopのScaffoldを作成

---
+++ app/models/shop.rb
@@ -0,0 +1,3 @@
+class Shop < ActiveRecord::Base
+  attr_accessible :description, :lines_summary, :name
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121023080328) do
+ActiveRecord::Schema.define(:version => 20121024044908) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(:version => 20121023080328) do
     t.datetime "updated_at",                 :null => false
     t.integer  "price",       :default => 0
     t.string   "image"
+  end
+
+  create_table "shops", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.text     "lines_summary"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
   end
 
 end
