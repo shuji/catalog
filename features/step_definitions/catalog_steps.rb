2704258386e39df11f7b4dfa24d3bfcbbdcf14b7
Shuji Watanabe <shuji.w6e@gmail.com>
2012-10-24 13:47:10 +0900

    [#38317601] テストシナリオを追加「管理ページから店舗を登録する」

--- features/step_definitions/catalog_steps.rb
+++ features/step_definitions/catalog_steps.rb
@@ -4,6 +4,10 @@
   visit "/products"
 end
 
+前提 /^店舗管理ページを開いている$/ do
+  visit "/shops"
+end
+
 前提 /^"(.*?)"をクリックする$/ do |link_text|
   click_link link_text
 end
@@ -16,10 +20,25 @@ end
   click_on 'Save'
 end
 
+もし /^以下の内容で店舗を登録する:$/ do |table|
+  product_info = table.hashes[0]
+  fill_in 'shop_name', with: product_info['店舗名']
+  fill_in 'shop_description', with: product_info['説明']
+  fill_in 'shop_lines_summary', with: product_info['取扱商品概要']
+  click_on 'Save'
+end
+
 ならば /^"(.*?)"が商品詳細ページに表示されていること$/ do |product_name|
   visit url_for(Product.where(name: product_name).first)
   within 'p.name' do
     page.should have_content(product_name)
+  end
+end
+
+ならば /^"(.*?)"が店舗詳細ページに表示されていること$/ do |shop_name|
+  visit url_for(Shop.where(name: shop_name).first)
+  within 'p.name' do
+    page.should have_content(shop_name)
   end
 end
 
@@ -32,5 +51,11 @@ end
 ならば /^価格が"(.*?)"となっていること$/ do |product_price|
   within 'p.price' do
     page.should have_content(product_price)
+  end
+end
+
+ならば /^取扱商品概要が"(.*?)"となっていること$/ do |lines_summary|
+  within 'p.lines_summary' do
+    page.should have_content(lines_summary)
   end
 end
