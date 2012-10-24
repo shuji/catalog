57c6247dfdcbec05f7ebd8b799b5f846c238a402
Shuji Watanabe <shuji.w6e@gmail.com>
2012-10-24 10:46:14 +0900

    [#38241015] 必須チェックを追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :name, :price, :image, :image_cache
   mount_uploader :image, ProductImageUploader
+
+  validates_presence_of :name, :description, :price
+
 end
