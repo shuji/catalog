6caf2cfb545a749e9a6d249137d7e88ff494f39f
Shuji Watanabe <shuji.w6e@gmail.com>
2012-10-24 11:16:37 +0900

    [#38241015] 価格は100円単位とする

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,15 @@ class Product < ActiveRecord::Base
   mount_uploader :image, ProductImageUploader
 
   validates_presence_of :name, :description, :price
-  validates_numericality_of :price, :only_integer => true, :greater_than => 0 
+  validates_numericality_of :price, only_integer: true, greater_than: 0 
   validates_uniqueness_of :name
+  validate :price_must_be_a_multiple_of_hundreds
+
+  private
+  def price_must_be_a_multiple_of_hundreds
+    unless price % 100 == 0
+      errors.add :price, "must be a multiple of hundreds."
+    end
+  end
+
 end
